e05a71ec80af567b255f65e2184183b7bbc4de80
Ryan Wright <rwright@student.42.us.org>
2019-03-12 19:48:48 -0700

    Begin work on printf again

---
+++ src/ft_printf.c
@@ -0,0 +1,66 @@
+#include "ft_printf.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int		convert(const char **format, va_list ap)
+{
+	t_fmt			*fmt;
+	int				i;
+	const t_able	table[] = {	{"c", conv_char}, {"s", conv_string}, {"p", conv_pointer},
+								{"di", conv_int}, {"ouxX", conv_uint}};
+
+	i = sizeof(table) / sizeof(t_able);
+	while (i--)
+	{
+		if (ft_strchr(table[i].keys, **format))
+			return (table[i].func(format, ap));
+	}
+	return (0);
+}
+
+int		next_section(const char **format, va_list ap)
+{
+	int offset;
+	int len;
+
+	offset = -1;
+	len = 0;
+	while (*format[++offset])
+	{
+		if (*format[offset] == '%')
+		{
+			write(1, *format, offset);
+			len = offset;
+			(*format) += ++offset;
+			return (len + convert(format, ap));
+		}
+	}
+	if (offset)
+	{
+		write(1, *format, offset);
+		(*format) += offset;
+	}
+	return (offset);
+}
+
+int		ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		len;
+
+	va_start(ap, format);
+	len = 0;
+	while (*format)
+		len += next_section(&format, ap);
+	va_end(ap);
+	return (len);
+}
+
+// While String
+//  print everything up to the next mod
+//  print conversion
+
+
+//  a
+// 01234567
+// 10
---
+++ src/parse.c
@@ -0,0 +1,58 @@
+#include "ft_printf.h"
+
+static void get_flags(t_fmt *fmt, const char **format)
+{
+	while (ft_strchr("#0- +'", **format))
+	{
+		if (ft_strlen(fmt->flags) == 6 || ft_strchr(fmt->flags, **format))
+			continue;
+		ft_strncat(fmt->flags, *format, 1);
+	}
+}
+
+static void get_width(t_fmt *fmt, const char **format)
+{
+	if (ft_isdigit(**format))
+	{
+		fmt->width = ft_atoi(*format);
+		while (ft_isdigit(**format))
+			(*format)++;
+	}
+}
+
+static void get_precision(t_fmt *fmt, const char **format)
+{
+	fmt->precision = -1;
+	if (**format == '.')
+	{
+		(*format)++;
+		if (!ft_isdigit(**format))
+			fmt->precision = 0;
+		else if (**format != '-')
+			fmt->precision = ft_atoi(**format);
+	}
+	while (**format == '-' || ft_isdigit(**format))
+		(*format)++;
+}
+
+static void get_len(t_fmt *fmt, const char **format)
+{
+	while (ft_strchr("lh", **format))
+	{
+		if (ft_strlen(fmt->len) == 2)
+			continue;
+		ft_strcat(fmt->len, *format, 1);
+	}
+}
+
+t_fmt *parse_fmt(const char **format)
+{
+	t_fmt *fmt;
+
+	fmt = ft_memalloc(sizeof(t_fmt));
+	get_len(fmt, format);
+	get_width(fmt, format);
+	get_precision(fmt, format);
+	get_len(fmt, format);
+	return (fmt);
+}
